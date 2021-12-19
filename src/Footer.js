7abb14df5a2093a71589a755bde214d970040f25
Gaurav <U20EC179@eced.svnit.ac.in>
2021-12-19 13:21:39 +0530

    done with homePage

--- src/Footer.js
+++ src/Footer.js
@@ -3,11 +3,12 @@ import React from "react";
 export const Footer = () => {
     let footstyle = {
         position : "absolute",
+      
         down : "0vh",
         width : "100%"
     }
   return (
-    <div className="bg-dark text-light py-3" style={footstyle}>
+    <div className="bg-light text-success " style={footstyle}>
       <p className="text-center"> &copy; Eduvibe </p>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,83 @@
-import React from 'react'
-import img1 from './home-img1.png'
-import img2 from './home-img2.png'
-import img3 from './home-img3.png'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import img1 from "./home-img1.png";
+import img2 from "./home-img2.png";
+import img3 from "./home-img3.png";
+import "./home.css";
 export function Home() {
-    let title = {
-        width: "1366px",
-        height: "81px",
-        background: "white",
-        boxShadow: "0px 1px 50px rgba(0,0,0,0.07)",
-      };
-      let logo = {
-        color: "#46F481",
-        fontFamily: "inherit",
-        fontSize: "24px",
-        fontWeight: 700,
-        fontStyle: "normal",
-        letterSpacing: "0.04em"
-      };
-      let img = {
-        display: "block",
-        marginLeft: "auto",
-        marginRight: "auto",
-        width: "100%"
-      }
-     
-    return (
-        <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light" style={title}>
-        <div class="container-fluid">
-          <p style={logo}>Eduvibe</p>
+  let navStyle = {
+    width: "1366px",
+    height: "81px",
+    background: "white",
+    boxShadow: "0px 1px 50px rgba(0,0,0,0.07)",
+  };
+
+  return (
+    <>
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-light"
+        style={navStyle}
+      >
+        <div className="container-fluid">
+          <p className="main-logo">Eduvibe</p>
         </div>
-       </nav>
-       <div>
-       <img className="img1" src={img1} style={img}/>
-       <img className="img2" src={img2} style={img}/>
-       <img className="img3" src={img3} style={img}/>
-       </div>
+      </nav>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={img1} className="d-block w-100" alt="..." />
+            <Link className="carousel-content " id="img1" to="/login">
+              <div>Login</div>
+            </Link>
+            <p className="carousel-content-2" id="home-content1">
+              Don't have an account? <Link to="#">Sign up for free</Link>
+            </p>
+          </div>
+          <div className="carousel-item">
+            <img src={img2} className="d-block w-100" alt="..." />
+            <Link className="carousel-content " id="img2" to="/login">
+              <div>Login</div>
+            </Link>
+            <p className="carousel-content-2">
+              Don't have an account? <Link to="#">Sign up for free</Link>
+            </p>
+          </div>
+          <div className="carousel-item">
+            <img src={img3} className="d-block w-100" alt="..." />
+            <Link className="carousel-content" id="img3" to="/login">
+              <div>Login</div>
+            </Link>
+            <p className="carousel-content-2" id="home-content-3">
+              Don't have an account? <Link to="#">Sign up for free</Link>
+            </p>
+            <div className="dots">...</div>
+          </div>
         </div>
-    )
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
